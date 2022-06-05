c1b2de4bc8086df5ecd26fb5f47e01716a52437e
Андрей Лиханов <mogueta63@gmail.com>
2022-06-05 12:56:09 +0500

    header for mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import { Switch } from 'react-router-dom';
@@ -15,22 +15,44 @@ import logo from '../images/Logo.svg';
 // header__button-icon - иконка меню
 // header__button-icon_close - вместо меню крестик
 
+
+
 const Header = ({ email, onSignOut, loggedIn }) => {
+
+  const [clicked, setClicked] = useState(false);
+
+  function handlerClass() {
+    if (!clicked) setClicked(true);
+    else setClicked(false);
+  }
+
+  function handleSignOut() {
+    setClicked(false);
+    onSignOut();
+  }
+
   return (
     <header className="header">
+      <div className={clicked ? 'header__drop-menu' : 'header__drop-menu_unactive'}>
+        <Route exact path="/">
+          <h3 className={clicked ? 'header__drop-menu_email' : 'header__email_unactive'}>{email}</h3>
+          <Link to="/" className={clicked ? 'header__drop-menu_logout header__login-button_logged-in' : ''} onClick={handleSignOut}>Выйти</Link>
+        </Route>
+      </div>
       <div className='header__main-header-wrapper'>
         <img src={logo} className="header__logo" alt="Логотип проекта" />
         <div className='header__wrapper'>
           <Switch>
             <Route exact path="/sign-up">
-              <Link to="/sign-in" className="header__login-button">Войти</Link>
+              <Link to="/sign-in" className={ loggedIn ? '' : 'header__login-button' }>Войти</Link>
             </Route>
             <Route exact path="/sign-in">
-              <Link to="/sign-up" className="header__login-button">Регистрация</Link>
+              <Link to="/sign-up" className={ loggedIn ? '' : 'header__login-button' }>Регистрация</Link>
             </Route>
             <Route exact path="/">
               <h3 className={loggedIn ? 'header__email_unactive' : 'header__email'}>{email}</h3>
-              <Link to="/" className="header__login-button header__login-button_logged-in" onClick={onSignOut}>Выйти</Link>
+              <Link to="/" className="header__logout-button header__login-button_logged-in" onClick={handleSignOut}>Выйти</Link>
+              <button className='header__button-icon' onClick={handlerClass} />
             </Route>
           </Switch>
         </div>
@@ -44,6 +66,9 @@ export default Header;
 
 
 /*
+className="header__login-button header__login-button_logged-in"
+
+
 <div className='header__login-button_unactive' >
         <h3 className="header__email">{props.email}</h3>
         <button className="header__login-button">
